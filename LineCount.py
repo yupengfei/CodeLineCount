948c91c7d56ad71f1d4952420a981326c54f2738
yupengfei <yupengfei@foxmail.com>
2015-03-25 15:47:28 +0800

    add coding

--- LineCount.py
+++ LineCount.py
@@ -1,10 +1,11 @@
 import os
+import codecs
 
-suffixList = ['java', 'xml', 'go', 'h', 'cpp', 'qml', 'js', 'html']
+suffixList = ['java', 'go', 'h', 'cpp', 'qml', 'js', 'html']
 
 countMap = {}
 
-def analysisFolder(path):
+def analysisFolder(path, coding):
 	fileList = os.listdir(path)
 	for fileItem in fileList:
 		# print(fileItem)
@@ -13,16 +14,16 @@ def analysisFolder(path):
 				splitList = fileItem.split(".")
 				if splitList[-1] in suffixList:
 					print(fileItem)
-					fp = open(path + '/' + fileItem)
+					fp = codecs.open(path + '/' + fileItem, encoding=coding)
 					lines = fp.readlines()
 					if splitList[-1] in countMap:
 						countMap[splitList[-1]] += len(lines)
 					else:
 						countMap[splitList[-1]] = len(lines)
 		else:
-			analysisFolder(path + '/'+ fileItem)
+			analysisFolder(path + '/'+ fileItem, coding)
 					# print(type(fileItem))
 
 if __name__ == '__main__':
-	analysisFolder('/home/developer/Go/src/CGWorldlineWeb')
+	analysisFolder('/home/developer/Downloads/i3/A_SRC', 'GB18030')
 	print(countMap)
